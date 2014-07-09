fef5403929fdb374a511c9e2386690244f87360a
Rick Fletcher <fletch@pobox.com>
2014-07-09 00:06:33 -0400

    Simplify apache::modules::evasive even more

--- manifests/modules/evasive.pp
+++ manifests/modules/evasive.pp
@@ -3,16 +3,7 @@
 # Manage Apache mod_evasive
 #
 class apache::modules::evasive {
-  include apache
-
-  file { "/etc/apache2/mods-available/evasive.load":
-    ensure  => present,
-    content => "LoadModule evasive20_module /usr/lib/apache2/modules/mod_evasive20.so\n",
-    notify  => $apache::manage_service_autorestart,
-    require => Package['apache'],
-  } ->
-
-  apache::module { 'evasive':
-    install_package => true,
+  apache::module { 'mod-evasive':
+    install_package => 'evasive',
   }
 }
