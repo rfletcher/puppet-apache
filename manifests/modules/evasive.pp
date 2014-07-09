d266f46686c606ec73082892654f45222d8dbc41
Rick Fletcher <fletch@pobox.com>
2014-07-08 23:31:26 -0400

    Simplify apache::module::evasive a bit

--- manifests/modules/evasive.pp
+++ manifests/modules/evasive.pp
@@ -3,16 +3,13 @@
 # Manage Apache mod_evasive
 #
 class apache::modules::evasive {
-  # the package fails to install a load file, so we have to do a little extra work
-  $mods_dir = "/etc/apache2/mods-available"
+  include apache
 
-  exec { "mkdir -p ${mods_dir}":
-    creates => $mods_dir,
-  } ->
-
-  file { "${mods_dir}/evasive.load":
+  file { "/etc/apache2/mods-available/evasive.load":
     ensure  => present,
-    content => 'LoadModule evasive20_module /usr/lib/apache2/modules/mod_evasive20.so',
+    content => "LoadModule evasive20_module /usr/lib/apache2/modules/mod_evasive20.so\n",
+    notify  => $apache::manage_service_autorestart,
+    require => Package['apache'],
   } ->
 
   apache::module { 'evasive':
