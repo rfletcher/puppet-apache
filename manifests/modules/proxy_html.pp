e10b7a7125ff2b5d160051b880de966ce108d5b6
Rick Fletcher <fletch@pobox.com>
2016-05-16 10:01:44 -0400

    Add missing dependency to mod_proxy_html

--- manifests/modules/proxy_html.pp
+++ manifests/modules/proxy_html.pp
@@ -3,6 +3,8 @@
 # Manage Apache mod_proxy_html
 #
 class apache::modules::proxy_html {
+  include ::apache::modules::proxy
+
   apache::module { 'proxy_html':
     ensure          => present,
     install_package => 'proxy-html',
